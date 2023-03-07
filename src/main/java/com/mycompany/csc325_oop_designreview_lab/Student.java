88aee33c474648be5a508642fa65893a85b96a0d
George-Macbook <nakagk@farmingdale.edu>
2023-03-07 18:58:20 -0500

    ToDo 2

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,10 +9,23 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human{
-	// ToDo 1: Make this class a child of Person
-	// ToDo 1: Make this class a child of Human
+    
+    private String address;
+    
+    public Student(String name, short age) {
+        super(name, age);
+        address = "";
+    }
 
-	// ToDo 2: Fix the resulting errors
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
