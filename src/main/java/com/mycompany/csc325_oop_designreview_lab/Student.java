0bb41daf802942ee9cfda1f11e6f217ad46b3f28
George-Macbook <nakagk@farmingdale.edu>
2023-03-07 19:05:47 -0500

    ToDo 4

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -13,33 +13,59 @@ public class Student extends Human{
     private String address;
     private int gpa;
     
+    /**
+     * Constructor with name and age
+     * @param name
+     * @param age 
+     */
     public Student(String name, short age) {
         super(name, age);
         address = "";
     }
     
+    /**
+     * Constructor with name, age, and gpa
+     * @param name
+     * @param age
+     * @param gpa 
+     */
     public Student(String name, short age, int gpa) {
         super(name, age);
         this.gpa = gpa;
         address = "";
     }
 
+    /**
+     * Gets the address.
+     * @return 
+     */
     @Override
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Sets the address.
+     * @param address 
+     */
     @Override
     public void setAddress(String address) {
         this.address = address;
     }
     
+    /**
+     * Gets the GPA
+     * @return 
+     */
     public int getGPA() {
         return gpa;
     }
 
+    /**
+     * Sets GPA
+     * @param gpa 
+     */
     public void setGPA(int gpa) {
         this.gpa = gpa;
     }
-	// ToDo 4: Add comments to your code
 }
