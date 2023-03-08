eea1cb812d357e7ae7d08c819a75b9dadfb39329
George-Macbook <nakagk@farmingdale.edu>
2023-03-07 19:09:17 -0500

    ToDo 5/6

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -11,38 +11,40 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class MainClass {
 
  public static void main(String[] args) {
-		// ToDo 5: Fix the error
+        // ToDo 5: Fix the error
 
-		// ToDo 6: Fix the constructor of Student class
+        // ToDo 6: Fix the constructor of Student class
 
-                // Todo 7: Create two classes for Freshman and Senior 
+        // Todo 7: Create two classes for Freshman and Senior 
 
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+        // ToDo 8: The senior class should have a minimum of 85 credits  
 
-		// ToDo 7: Add a toString method for Student class
-		// ToDo 9: Add a toString method for Freshman class
+        // ToDo 7: Add a toString method for Student class
+        // ToDo 9: Add a toString method for Freshman class
 
-		Student std1= new Student("James", 20);
-                // ToDo 10: Add a toString method for Senior class
+        Student std1= new Student("James", 20);
+        // ToDo 10: Add a toString method for Senior class
 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
+        Freshman std2= new Student("James", 20, 12); // name, age, credits
 
-                Senior std2 = new Student("John", 30, 90);
+        Senior std3 = new Student("John", 30, 90);
 
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// ToDo 11: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
+        // ToDo 8: Set the gpa of the student using the scanner and user
+        // ToDo 11: Set the gpa of the student using the scanner and user
+        // 			input and then print the output.
 
-		System.out.println(std1);
+        System.out.println(std1);
 
-                System.out.println(std2);
+        System.out.println(std2);
+        
+        System.out.println(std3);
 
-		// ToDo 9: add comments and explain your code
-		// ToDo 12: add comments and explain your code
+        // ToDo 9: add comments and explain your code
+        // ToDo 12: add comments and explain your code
 
-		// ToDo 10: submit using a pull request.
-		// ToDo 13: submit using a pull request.
-	}
+        // ToDo 10: submit using a pull request.
+        // ToDo 13: submit using a pull request.
+    }
 
 }
 
