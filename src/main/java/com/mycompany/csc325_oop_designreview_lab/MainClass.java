0dfadcf9d5443b94e45e847306f5ca79b5c962f6
George-Macbook <nakagk@farmingdale.edu>
2023-03-10 23:11:18 -0500

    Todo 12

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -4,8 +4,6 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -15,26 +13,24 @@ public class MainClass {
 
     public static void main(String[] args) {
 
+        //Creating student, freshman and senior
         Student std1 = new Student("James", 20);
 
         Student std2 = new Freshman("James", 20, 12); // name, age, credits
 
-        Student std3;
-        std3 = new Senior("John", 30, 90);
+        Student std3 = new Senior("John", 30, 90);
 
+        //Scanning input for GPA
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter student 1's GPA");
         std1.setGPA(scnr.nextInt());
         System.out.println("Student 1 GPA: " + std1.getGPA());
 
+        //Printing out student data
         System.out.println(std1);
 
         System.out.println(std2);
 
         System.out.println(std3);
-
-        // ToDo 12: add comments and explain your code
-        // ToDo 13: submit using a pull request.
     }
-
 }
